d3014c5be55a765816b7f3b79915079464aac1ba
Dan23H <123597766+Dan23H@users.noreply.github.com>
2023-05-24 16:57:46 -0500

    modified:   routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,9 @@ const { validarCampos } = require('../middlewares/validar-campos')
 
 router.post('/newPedido', [
     check('id', 'El ID es obligatorio').not().isEmpty(),
+    check('id', 'El ID debe ser numérico').isNumeric(),
     check('fecha', 'La fecha es obligatoria').not().isEmpty(),
+    check('fecha', 'La fecha debe ser una fecha válida').isDate(),
     validarCampos
 ], nuevoPedido)
 
@@ -14,8 +16,11 @@ router.post(
     '/newCoordenada',
     [
         check('X', 'El valor de X es obligatorio').not().isEmpty(),
+        check('X', 'El valor de X es numérico').isNumeric(),
         check('Y', 'El valor de Y es obligatorio').not().isEmpty(),
+        check('Y', 'El valor de Y es numérico').isNumeric(),
         check('pedido_id', "El pedido_id es obligatorio").not().isEmpty(),
+        check('pedido_id', 'El pedido_id debe ser numérico').isNumeric(),
         validarCampos
     ],
     nuevaCoordenada)
